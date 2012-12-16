044e756ddaba7bf70e00337d8843497ecbb427bd
Arnon Yaari <wiggin15@yahoo.com>
2012-12-16 14:56:35 +0200

    small refactor to script entry points

--- src/infi/docopt_completion/docopt_completion.py
+++ src/infi/docopt_completion/docopt_completion.py
@@ -1,7 +1,7 @@
 import sys
-from bash import BashCompletion
-from zsh import OhMyZshCompletion, ZshPreztoCompletion
-from common import DocoptCompletionException, parse_params
+from .bash import BashCompletion
+from .zsh import OhMyZshCompletion, ZshPreztoCompletion
+from .common import DocoptCompletionException, parse_params
 
 def docopt_completion(cmd):
     completion_generators = [OhMyZshCompletion(),
@@ -10,26 +10,24 @@ def docopt_completion(cmd):
     generators_to_use = [generator for generator in completion_generators if generator.completion_path_exists()]
     
     if len(generators_to_use) == 0:
-        print "No completion paths found."
-        return 1
+        raise DocoptCompletionException("No completion paths found.")
     
-    try:
-        param_tree, option_help = parse_params(cmd)
-    except DocoptCompletionException, e:
-        print e.message
-        return 1
+    param_tree, option_help = parse_params(cmd)
         
     for generator in generators_to_use:
         generator.generate(cmd, param_tree, option_help)
-    
-    return 0
 
 def main():
     if len(sys.argv) != 2:
         print "Usage: {} <docopt-script>".format(sys.argv[0])
         return 1
     program, cmd = sys.argv
-    return docopt_completion(cmd)
+    try:
+        docopt_completion(cmd)
+    except DocoptCompletionException, e:
+        print e.mesage
+        return 1
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main())
